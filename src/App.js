1dcb7d98f51f35d784b9e07cb8764f28dde8cfc8
Dhasneem Beevi <122379925+Dhasneembeevi@users.noreply.github.com>
2023-04-01 16:12:36 +0530

    Implemented Edit functionality

--- src/App.js
+++ src/App.js
@@ -15,29 +15,44 @@ function App() {
     setText(text)
     setTodoId(_id)
   }
+  const handleAddOrUpdateTodo = () => {
+    if (isupdating) {
+      updatetodo(todoId, text, setToDo, setText, setisUpdating)
+    } 
+    else {
+      addtodo(text, setText, setToDo)
+    }
+  }
   return (
-    <div className="App">
-      <div className="container" >
-        <h1>To Do App</h1>
-        <div className="top">
+    <div className="app-container">
+      <div className="bg-image"></div>
+      <div className="todo-container">
+        <h1 className="title">To Do App</h1>
+        <div className="input-container">
           <input
             type="text"
             placeholder="Add to dos..."
             value={text}
-            onChange={(e) => setText(e.target.value)} />
-
-          <button onClick={isupdating ?
-            () => updatetodo(todoId, text, setToDo, setText, setisUpdating)
-            : () => addtodo(text, setText, setToDo)} > {isupdating ? "Update" : "Add"} </button>
+            onChange={(e) => setText(e.target.value)}
+            className="input-list-box"
+          />
+          <button onClick={handleAddOrUpdateTodo} className="todo-btn">
+            {isupdating ? "Update" : "Add"}
+          </button>
         </div>
-        <div className="list">
-          {todo.map((item) => <ToDo key={item._id} text={item.text}
-            updateMode={() => updateMode(item._id, item.text)}
-            deletetoDo={() => deletetodo(item._id, item.text)}
-          />)}
+        <div className="list-container">
+          {todo.map((item) => (
+            <ToDo
+              key={item._id}
+              text={item.text}
+              updateMode={() => updateMode(item._id, item.text)}
+              deletetoDo={() => deletetodo(item._id, setToDo)}
+            />
+          ))}
         </div>
       </div>
     </div>
-  );
+  )
 }
 export default App;
+
