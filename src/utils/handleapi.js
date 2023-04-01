195060b41c1bd78626bf71d8ad90fffe8fb05909
Dhasneem Beevi <122379925+Dhasneembeevi@users.noreply.github.com>
2023-04-01 16:16:07 +0530

    Added catch block to get route to handle errors

--- src/utils/handleapi.js
+++ src/utils/handleapi.js
@@ -8,16 +8,15 @@ const getAllToDo = (setToDo) => {
             console.log('data ----> ', data)
             setToDo(data)
         })
-
+        .catch((err) => console.log(err))
 }
 const addtodo = (text, setText, setToDo) => {
     axios
         .post(`${baseUrl}/save`, { text })
         .then(({ data }) => {
             console.log(data)
-            setToDo("")
+            setText("")
             getAllToDo(setToDo)
-
         })
         .catch((err) => console.log(err))
 }
@@ -37,6 +36,7 @@ const deletetodo = (_id, setToDo) => {
     axios
         .post(`${baseUrl}/delete`, { _id })
         .then(({ data }) => {
+            console.log(data)
             getAllToDo(setToDo)
         })
         .catch((err) => console.log(err))
